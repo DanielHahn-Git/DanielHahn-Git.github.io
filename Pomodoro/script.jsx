ef6509f220ac161c40c727681789e084afd5e49b
dahnkali@gmail.com <dahnkali@gmail.com>
2023-02-16 05:05:21 -0300

    ok

--- Pomodoro/script.jsx
+++ Pomodoro/script.jsx
@@ -1,247 +1,231 @@
-//"use strict";
+"use strict";
 
-const { useState, useEffect, useRef } = React;
-
-
+const {
+  useState,
+  useEffect,
+  useRef
+} = React;
 
 function App() {
   return (
     <CountDown />
   );
 }
-/* function App() {
-    return (
-        <React.StrictMode>
-            <CountDown />
-        </React.StrictMode>
-    );
-} */
-
 const CountDown = () => {
-  const [sessionLength, setSessionLength] = useState(2);
-  const [breakLength, setBreakLength] = useState(3);
-  const [contador, setContador] = useState(60);
-  const [countLabel, setCountLabel] = useState(true);
-  const [isRun, setIsRun] = useState(false);
-  const [reset, setReset] = useState(true);
-  const [play, setPlay] = useState(false);
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [timerState, setTimerState] = useState("stopped");
+  const [timerType, setTimerType] = useState("Session");
+  const [timer, setTimer] = useState(1500);
+  const [intervalId, setIntervalId] = useState("");
+  //const [play, setPlay] = useState(false);
   const audioRef = useRef(null);
-
-  const handlePlay = () => {
-    if (play) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-    }
-    setPlay(!play);
-  }
-
-
-  /* useEffect(() => {
-    if (countLabel === true) {
-      setContador(contador => sessionLength * 60);
-    }
-    return () => {
-      //console.log('session');
-    }
-  }, [sessionLength, countLabel]);
   
-  useEffect(() => {
-    if (countLabel === false) {
-      setContador(contador => breakLength * 60);
-    }
-    return () => {
-      //console.log('break');
-    }
-  }, [breakLength, countLabel]); */
-
-  useEffect(() => {
-    if (isRun) {
-      // console.log(`${contador}  contador 1`);
-      const intervalId = setInterval(() => {
-        // console.log(`${contador}  contador 2`);
-        // console.log(`${contador}  contador 2`);
-        let countAux = contador;
-        console.log(`${countAux}  countAux 2`);
-        setContador(contador => contador - 1);
-        console.log(`${contador}  contador 3`);
-      }, 100, [contador]);
-      // console.log(`${contador}  contador 4`);
-      return () => {
-        clearInterval(intervalId);
-        //console.log('session');
+  
+  const setBreakTime = (e) => {
+    if (timerState === 'stopped') {
+      let time = breakLength;
+      let op = e.target.value;
+      if (op === '-') {
+        setBreakLength(breakLength => breakLength <= 1 ?
+          breakLength = 1 :
+          breakLength - 1);
+          time <= 1 ? 
+          time = 1 : 
+          time = time - 1;
+      }
+      if (op === '+') {
+        setBreakLength(breakLength => breakLength >= 60 ? breakLength = 60 : breakLength + 1);
+          time >= 60 ? 
+          time = 60 : 
+          time = time + 1;
+      }
+      if (timerType === 'Break') {
+        setLengthTime(time);
       }
     }
-  }, [isRun]);
+  }
+  const setSessionTime = (e) => {
+    if (timerState === 'stopped') {
+      let time = sessionLength;
+      let op = e.target.value;
+      if (op === '-') {
+        setSessionLength(sessionLength => sessionLength <= 1 ? sessionLength = 1 : sessionLength - 1);
+          time <= 1 ? 
+          time = 1 : 
+          time = time - 1;
+      }
+      if (op === '+') {
+        setSessionLength(sessionLength => sessionLength >= 60 ? sessionLength = 60 : sessionLength + 1);
+          time >= 60 ? 
+          time = 60 : 
+          time = time + 1;
+      }
+      if (timerType === 'Session') {
+        setLengthTime(time);
+      }
+    }
+  }
+  
+  const setLengthTime = (time) => {
+    setTimer(timer => 60 * time);
+  }
+  
+  const timerControl = () => {
+    console.log(timerState);
+    if (timerState === "stopped") {
+      setTimerState(timerState => "running");
+      countDown();
+    }
+    if (timerState === "running") {
+      setTimerState(timerState => "stopped");
+      setIntervalId(intervalId => clearInterval(intervalId));
+    }
+  }
 
-  useEffect(() => {
-    if (contador === 0) {
-      setIsRun(!isRun);
-      setCountLabel(!countLabel);
-    }
-    return () => {
-      // console.log('contador');
-    }
-  }, [contador])
+  const countDown = () => {
+    setIntervalId(intervalId => setInterval(() => {
+      decrementTime();
+    }, 1000))
+  };
+
+  const decrementTime = () => {
+    setTimer(timer => timer - 1);
+  };
+
+  const timerTypeControl = () => {
+    var time = timer;
+    beep(time);
+    time < 0 && (setIntervalId(intervalId => clearInterval(intervalId)),
+    "Session" === timerType ? (changeTimerType(60 * breakLength, "Break"), countDown()) : (changeTimerType(60 * sessionLength, "Session"), countDown()));    
+  };
+
+  const changeTimerType = (e, t) => {
+    setTimer(timer => e);
+    setTimerType(timerType => t);
+  };
+  
+  const beep = (time) => {
+    0 === time && audioRef.current.play();
+  };
+  
+  const reset = () => {
+    setIntervalId(intervalId => clearInterval(intervalId));
+    setBreakLength(breakLength => 5);
+    setSessionLength(sessionLength => 25);
+    setTimerState(timerState => "stopped");
+    setTimerType(timerType => "Session");
+    setTimer(timer => 1500);
+    setIntervalId(intervalId => "");
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
+  };
+    
+  const printTime = () => {
+    if (timer < 0) return "00:00";
+    var m = Math.floor(timer / 60),
+      s = timer - 60 * m;
+    return (m = m < 10 ? "0" + m : m) + ":" + (s = s < 10 ? "0" + s : s)
+  }; 
 
 
   //console.log(`${sessionLength}  sessionLength`);
   //console.log(`${breakLength}  breakLength`);
-
-  console.log(`${contador} contador state`);
-  //console.log(`${countLabel} countLabel state`);
-  //console.log(`${printCountLabel(countLabel)} countLabel function`);
-  //console.log(`${isRun} isRun`);
+  //console.log(`${timer}  timer state render`);
+  //console.log(`${timerType} timerType state`);
+  //console.log(`${printCountLabel(timerType)} timerType function`);
+  //console.log(`${timerState} timerState`);
   return (
     <div className="frame">
-    <div className="header">
+      <div className="header">
       <h1 className="border">Pomodoro Timer</h1>
-    </div>
-    <div className="countdown">
+      </div>
+      <div className="countdown">
+      <audio id="beep" ref={audioRef} src="beep.mp3" controls={false} 
+      ></audio>
       <div className="count-title-div">
-        <h3 id="timer-label" className="count-title">{countLabel ? "Session" : "Break"}</h3>
-        <audio ref={audioRef} src="./beep.mp3" controls={false}></audio>
+      <h3 id="timer-label" className="count-title">{timerType}</h3>
       </div>
       <div className="count-div">
-        <span id="time-left" className="count-span">{`${Math.floor(contador / 60) < 10 ? "0" + Math.floor(contador / 60) : Math.floor(contador / 60) }:${contador % 60 < 10 ? "0" + contador % 60 : contador % 60}`}</span>
+      <span id="time-left" className="count-span" onChange={timerTypeControl()}>{printTime()}</span>
       </div>
-    </div>
-    <div className="control-count-div">
+      </div>
+      <div className="control-count-div">
       <button
-        id="start_stop"
-        className="play_pause ctrl-count"
-        type="button"
-        value={"Start"}
-        onClick={() => setIsRun(!isRun)}>
-        {isRun ? "Stop" : "Start"}
+      id="start_stop"
+      className="play_pause ctrl-count"
+      type="button"
+      onClick={timerControl}>{timerState == "stopped" ? "Start" : "Stop"}
       </button>
       <button
-        id="reset"
-        className="reset ctrl-count"
-        type="button"
-        value={"Reset"}
-        onClick={() => {
-          setSessionLength(2);
-          setBreakLength(3);
-          setContador(60);
-          setCountLabel(true);
-          setIsRun(false);
-        }}>Reset
+      id="reset"
+      className="reset ctrl-count"
+      type="button"
+      onClick={reset}
+      >Reset
       </button>
-    </div>
-    <div className="ctrl">
+      </div>
+      <div className="ctrl">
       <div className="inc-dec">
-        <div className="time-title">
-          <h4 id="session-label">Session Length</h4>
-        </div>
-        <div className="ctrl-time container">
-          <div className="view item">
-            <span id="session-length" className="skd-time">{sessionLength}</span>
-          </div>
-            <button
-              id="session-decrement"
-              type="button"
-              className="dec item"
-              title="dec"
-              onClick={() => {
-                if (isRun === true) return;
-                sessionLength > 1 ?
-                setSessionLength(sessionLength => sessionLength - 1) :
-                setSessionLength(sessionLength => 1);
-                if (countLabel === true) {
-                  contador > 60 ? setContador(contador => contador - 60) : setContador(contador => 60);
-                }
-              }}>-</button>
-            <button
-              id="session-increment"
-              type="button"
-              className="inc item"
-              title="inc"
-              onClick={() => {
-                if (isRun === true) return;
-                sessionLength < 60 ?
-                setSessionLength(sessionLength => sessionLength  + 1) :
-                setSessionLength(sessionLength => 60);
-                if (countLabel === true) { contador < 3600 ? setContador(contador => contador + 60) : setContador(contador => 3600);
-                }
-              }}>+</button>
-          </div>
-        </div>
-          <div className="inc-dec">
-            <div className="time-title">
-              <h4 id="break-label">Break Length</h4>
-            </div>
-            <div className="ctrl-time container">
-              <div className="view item"><span id="break-length" className="skd-time">{breakLength}</span>
-              </div>
-              <button
-                id="break-decrement"
-                type="button"
-                className="dec item"
-                title="dec"
-                onClick={() => {
-                  if (isRun === true) return;
-                  breakLength > 1 ?
-                  setBreakLength(breakLength => breakLength - 1) :
-                  setBreakLength(breakLength => 1);
-                  if (countLabel === false) {
-                    contador > 60 ? setContador(contador => contador - 60) : setContador(contador => 60);
-                  }
-                }}>-</button>
-              <button
-                id="break-increment"
-                type="button"
-                className="inc item"
-                title="inc"
-                onClick={() => {
-                  if (isRun === true) return;
-                  breakLength < 60 ?
-                  setBreakLength(breakLength => breakLength  + 1) :
-                  setBreakLength(breakLength => 60);
-              if (countLabel === false) {
-                contador < 3600 ? setContador(contador => contador + 60) : setContador(contador => 3600);
-              }
-            }}>+</button>
-          </div>
-        </div>
+      <div className="time-title">
+      <h4 id="session-label">Session Length</h4>
       </div>
-    </div>
+      <div className="ctrl-time container">
+      <div className="view item">
+      <span id="session-length" className="skd-time">{
+      sessionLength}
+      </span>
+      </div>
+      <button
+      id="session-decrement"
+      type="button"
+      className="dec item"
+      title="dec"
+      value={'-'}
+      onClick={setSessionTime}
+      >-</button>
+      <button
+      id="session-increment"
+      type="button"
+      className="inc item"
+      title="inc"
+      value={'+'}
+      onClick={setSessionTime}
+      >+</button>
+      </div>
+      </div>
+      <div className="inc-dec">
+      <div className="time-title">
+      <h4 id="break-label">Break Length</h4>
+      </div>
+      <div className="ctrl-time container">
+      <div className="view item"><span id="break-length" className="skd-time">{
+      breakLength}
+      </span>
+      </div>
+      <button
+      id="break-decrement"
+      type="button"
+      className="dec item"
+      title="dec"
+      value={'-'}
+      onClick={setBreakTime}
+      >-</button>
+      <button
+      id="break-increment"
+      type="button"
+      className="inc item"
+      title="inc"
+      value={'+'}
+      onClick={setBreakTime}
+      >+</button>
+      </div>
+      </div>
+      </div>
+      </div>
   );
 }
-
 const element = <App />;
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(element);
-
-
-
-/* 
-function contagem() {
-  console.log('1 inicio contagem  ' + contador);
-  let num = contador;
-  if (isRun) {
-  console.log('2 inicio isRun true  ' + contador);
-    let intervalId = setInterval(() => {
-  console.log('3 inicio setInterval  ' + contador);
-      if (stoped) {
-        clearInterval(intervalId);
-        console.log('★ stoped');
-      } 
-      if (num === 0) {
-        //setContador(num);
-        console.log('4 if num === 0  ' + contador);
-        clearInterval(intervalId);
-        console.log('5 after clearInterval  ' + contador);
-      } else {
-        console.log('6 inicio else num--  ' + contador);
-        num--;
-        setContador(num);
-        console.log('7 fim else num--  ' + contador);
-      }
-        console.log('8 fim setInterval  ' + contador);
-    }, 100);
-        console.log('9 fim if isRun true  ' + contador);
-  }
-        console.log('10 fim contagem  ' + contador);
-} */
