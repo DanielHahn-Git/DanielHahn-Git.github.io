53633dc03e8a63893d939fc6ce62e5211918dd50
dahnkali@gmail.com <dahnkali@gmail.com>
2023-04-13 22:07:01 -0300

    hdhd

---
+++ Conversor/script.jsx
@@ -0,0 +1,14 @@
+const { useState, useEffect, useRef } = React;
+
+function App() {
+  return (
+    <h1 />
+    )
+}
+
+
+
+const element = <App />;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(element);
